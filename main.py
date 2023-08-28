68e93d2a0a7b390783ef26c19d79f5dc0fcce840
Bruno Saboya <brunobs6@al.insper.edu.com.br>
2023-08-28 16:41:58 -0300

    Roteiro um

--- main.py
+++ main.py
@@ -1,41 +1,88 @@
 import sys
 
-def evaluate_expression(expression):
-    terms = expression.split('+')
-    total = 0
-    
-    for term in terms:
-        if '-' in term:
-            sub_terms = term.split('-')
-            sub_total = int(sub_terms[0])
-            for sub_term in sub_terms[1:]:
-                sub_total -= int(sub_term)
-            total += sub_total
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+class Tokenizer:
+    def __init__(self, source):
+        self.source = source.replace(" ", "").strip()
+        self.position = 0
+        self.next = None
+        
+    def selectNext(self):
+        if self.position < len(self.source):
+            current_char = self.source[self.position]
+
+            if current_char.isdigit():
+                value = ""
+                while self.position < len(self.source) and self.source[self.position].isdigit():
+                    value += self.source[self.position]
+                    self.position += 1
+                self.next = Token(int(value), "INT")
+
+            elif current_char == '+':
+                self.next = Token("+", "PLUS")
+                self.position += 1
+
+            elif current_char == '-':
+                self.next = Token("-", "MINUS")
+                self.position += 1
+
+            else:
+                raise ValueError(f"Unexpected character: {current_char}")
         else:
-            total += int(term)
-    
-    return total
+            self.next = Token("", "EOF")
 
-def handle_error(error_message):
-    print("Erro:", error_message, file=sys.stderr)
+class Parser:
 
-if len(sys.argv) != 2:
-    handle_error("Input vazio")
-else:
-    expression = sys.argv[1]
-    expression = ''.join(expression.split())  # Remove todos os espaços
-    
-    if not expression:
-        handle_error("Input vazio")
-    elif expression.startswith('+') or expression.endswith('+'):
-        handle_error("String começa ou termina com uma operação")
-    elif "++" in expression or "--" in expression or "+-" in expression or "-+" in expression:
-        handle_error("Mais de uma operação em sequência")
-    elif any(op in expression for op in "+-"):
-        try:
-            result = evaluate_expression(expression)
-            print(result)
-        except:
-            handle_error("Expressão inválida")
-    else:
-        handle_error("Espaço sem operação entre números")
+    tokenizer = None
+
+    @staticmethod
+    def parseExpression():
+
+        if Parser.tokenizer.next.type == "INT":
+            result = Parser.tokenizer.next.value
+            Parser.tokenizer.selectNext()
+
+            while Parser.tokenizer.next.type in ["PLUS", "MINUS"]:
+                if Parser.tokenizer.next.value == "+":
+                    Parser.tokenizer.selectNext()
+
+                    if Parser.tokenizer.next.type == "INT":
+                        result += Parser.tokenizer.next.value
+                        Parser.tokenizer.selectNext()
+                    else:
+                        raise ValueError("Expected INT after +")
+                elif Parser.tokenizer.next.value == "-":
+                    Parser.tokenizer.selectNext()
+
+                    if Parser.tokenizer.next.type == "INT":
+                        result -= Parser.tokenizer.next.value
+                        Parser.tokenizer.selectNext()
+                    else:
+                        raise ValueError("Expected INT after -")
+                else:
+                    raise ValueError("Expected + or - operator")
+
+            return result
+        else:
+            raise ValueError("Expected INT at the beginning of expression")
+
+    @staticmethod
+    def run(code):
+        tokenizer = Tokenizer(code)
+        parser = Parser(tokenizer)
+
+        result = parser.parseExpression()
+
+        if parser.tokenizer.next is not None:
+            raise SyntaxError("Parsing não completado. Token inválido ou não esperado.")
+
+        return result
+
+if __name__ == "__main__":
+    p = Parser()
+    teste = p.run(sys.argv[1])
+    print(teste)
